9b3fb473e7ba1da88ed67361609d98c90381df59
Saurabh Shukla <99032013+Saurabh13042004@users.noreply.github.com>
2025-08-30 03:21:20 +0530

    server-conf

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(helmet({
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.CORS_ORIGIN?.split(',') || ['http://localhost:3000', 'http://localhost:5173'],
+  origin: process.env.CORS_ORIGIN?.split(',') || ['http://localhost:3000', 'http://localhost:5173', 'https://www.kashgrip.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
